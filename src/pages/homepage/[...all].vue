daeac04ce13d8468371c45d74321660cbb228f24
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 19:20:23 +0200

    init commit

---
+++ src/pages/homepage/[...all].vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex h-full items-center justify-center">
+    <NotFound
+      subtitle="Sorry, we couldn't find your page."
+      title="Oops, something went wrong."
+      top="404"
+    />
+  </div>
+</template>
